182b0ba5dd2348f8e3a2d72d0c02ed040e953f0b
Kevin Herrera <kevin@herrera.io>
2013-02-08 15:39:39 -0800

    Adding current source.

---
+++ src/lib/Herrera/DateInterval/DateInterval.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Herrera\DateInterval;
+
+/**
+ * Adds functionality to the DateInterval class.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class DateInterval extends \DateInterval
+{
+    /**
+     * Number of seconds in a minute.
+     *
+     * @var integer
+     */
+    const SECONDS_MINUTE = 60;
+
+    /**
+     * Number of seconds in an hour.
+     *
+     * @var integer
+     */
+    const SECONDS_HOUR = 3600;
+
+    /**
+     * Number of seconds in a day.
+     *
+     * @var integer
+     */
+    const SECONDS_DAY = 86400;
+
+    /**
+     * The number of seconds in a month.
+     *
+     * Based on a 30.4368 day month, with the product rounded.
+     *
+     * @var integer
+     */
+    const SECONDS_MONTH = 2629740;
+
+    /**
+     * The number of seconds in a year.
+     *
+     * Based on a 365.2416 day year, with the product rounded.
+     *
+     * @var integer
+     */
+    const SECONDS_YEAR = 31556874;
+
+    /**
+     * The date properties.
+     *
+     * @var array
+     */
+    private static $date = array('y' => 'Y', 'm' => 'M', 'd' => 'D');
+
+    /**
+     * The time properties.
+     *
+     * @var array
+     */
+    private static $time = array('h' => 'H', 'i' => 'M', 's' => 'S');
+
+    /**
+     * Returns the interval specification.
+     *
+     * @return string The interval specification.
+     */
+    public function __toString()
+    {
+        return self::toSpec($this);
+    }
+
+    /**
+     * Returns the total number of seconds in the interval specification.
+     *
+     * @param DateInterval $interval The date interval.
+     *
+     * @return string The number of seconds.
+     */
+    public function toSeconds(\DateInterval $interval = null)
+    {
+        if ((null === $interval) && isset($this)) {
+            $interval = $this;
+        }
+
+        $seconds = (string) $interval->s;
+
+        if ($interval->i) {
+            $seconds = bcadd($seconds, bcmul($interval->i, self::SECONDS_MINUTE));
+        }
+
+        if ($interval->h) {
+            $seconds = bcadd($seconds, bcmul($interval->h, self::SECONDS_HOUR));
+        }
+
+        if ($interval->d) {
+            $seconds = bcadd($seconds, bcmul($interval->d, self::SECONDS_DAY));
+        }
+
+        if ($interval->m) {
+            $seconds = bcadd($seconds, bcmul($interval->m, self::SECONDS_MONTH));
+        }
+
+        if ($interval->y) {
+            $seconds = bcadd($seconds, bcmul($interval->y, self::SECONDS_YEAR));
+        }
+
+        return $seconds;
+    }
+
+    /**
+     * Returns the interval specification.
+     *
+     * @param DateInterval $interval The date interval.
+     *
+     * @return string The interval specification.
+     */
+    public function toSpec(\DateInterval $interval = null)
+    {
+        if ((null === $interval) && isset($this)) {
+            $interval = $this;
+        }
+
+        $string = 'P';
+
+        foreach (self::$date as $property => $suffix) {
+            if ($interval->{$property}) {
+                $string .= $interval->{$property} . $suffix;
+            }
+        }
+
+        if ($interval->h || $interval->i || $interval->s) {
+            $string .= 'T';
+
+            foreach (self::$time as $property => $suffix) {
+                if ($interval->{$property}) {
+                    $string .= $interval->{$property} . $suffix;
+                }
+            }
+        }
+
+        return $string;
+    }
+}
