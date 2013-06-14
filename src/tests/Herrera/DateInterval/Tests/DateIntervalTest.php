c2e0e41c2202ad0fdd0836d7d42515a1bce58301
Kevin Herrera <kevin@herrera.io>
2013-06-14 13:38:17 -0700

    Souping up fromSecondsUsingDays() test.

--- src/tests/Herrera/DateInterval/Tests/DateIntervalTest.php
+++ src/tests/Herrera/DateInterval/Tests/DateIntervalTest.php
@@ -2,11 +2,56 @@
 
 namespace Herrera\DateInterval\Tests;
 
+use DateTime;
 use Herrera\DateInterval\DateInterval;
 use PHPUnit_Framework_TestCase as TestCase;
 
 class DateIntervalTest extends TestCase
 {
+    public function getDiffs()
+    {
+        $list = array(
+            array(
+                '1',
+                new DateTime('2010-01-01 00:00:01'),
+                new DateTime('2010-01-01 00:00:00'),
+            ),
+            array(
+                '60',
+                new DateTime('2010-01-01 00:01:00'),
+                new DateTime('2010-01-01 00:00:00'),
+            ),
+            array(
+                '3600',
+                new DateTime('2010-01-01 01:00:00'),
+                new DateTime('2010-01-01 00:00:00'),
+            ),
+            array(
+                '86400',
+                new DateTime('2010-01-02 00:00:00'),
+                new DateTime('2010-01-01 00:00:00'),
+            ),
+            array(
+                null,
+                new DateTime('2010-02-01 00:00:00'),
+                new DateTime('2010-01-01 00:00:00'),
+            ),
+            array(
+                null,
+                new DateTime('2011-01-01 00:00:00'),
+                new DateTime('2010-01-01 00:00:00'),
+            ),
+        );
+
+        $month = $list[4][1]->diff($list[4][2]);
+        $list[4][0] = $month->days * DateInterval::SECONDS_DAY;
+
+        $year = $list[5][1]->diff($list[5][2]);
+        $list[5][0] = $year->days * DateInterval::SECONDS_DAY;
+
+        return $list;
+    }
+
     public function getSeconds()
     {
         return array(
@@ -46,10 +91,10 @@ class DateIntervalTest extends TestCase
 
     public function testFromSecondsCompare()
     {
-        $a = DateInterval::fromSeconds('2056600500');
-        $b = new DateInterval ('P60Y60M60DT60H60M60S');
+        $seconds = DateInterval::fromSeconds('2056600500');
+        $interval = new DateInterval('P60Y60M60DT60H60M60S');
 
-        $this->assertEquals($a->toSeconds(), $b->toSeconds());
+        $this->assertEquals($seconds->toSeconds(), $interval->toSeconds());
     }
 
     public function testToString()
@@ -66,15 +111,34 @@ class DateIntervalTest extends TestCase
 
         $this->assertEquals($seconds, $interval->toSeconds());
     }
-    
-    public function testToSecondsUsingDays()
+
+    /**
+     * @dataProvider getDiffs
+     *
+     * @param string  $seconds
+     * @param DateTime $left
+     * @param DateTime $right
+     */
+    public function testToSecondsUsingDays($seconds, $left, $right)
     {
-        $date1 = new \DateTime('2012-06-10 12:30:00');
-        $date2 = new \DateTime('2013-06-10 13:35:05');
-        $intervalfromDiff = $date1->diff($date2);
-        
         $interval = new DateInterval('PT0S');
-        $this->assertEquals(31539905, $interval->toSecondsUsingDays($intervalfromDiff));
+
+        $this->assertEquals(
+            $seconds,
+            $interval->toSecondsUsingDays($left->diff($right))
+        );
+    }
+
+    public function testToSecondsUsingDaysNotSet()
+    {
+        $interval = new DateInterval('PT0S');
+
+        $this->setExpectedException(
+            'InvalidArgumentException',
+            'The "days" property is not set.'
+        );
+
+        DateInterval::toSecondsUsingDays($interval);
     }
 
     /**
